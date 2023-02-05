994af4a3fcb3e12e2537044c3c080ff774946e8e
KyleLiuCat <kyle.liu.coffeecat@gmail.com>
2023-02-06 00:15:25 +0800

    Ver 0.5
    ublox.h unit test done

--- Firmware/AllSystem_PMU_V2.2_ublox/AllSystem_PMU_V2.2_ublox.ino
+++ Firmware/AllSystem_PMU_V2.2_ublox/AllSystem_PMU_V2.2_ublox.ino
@@ -27,120 +27,13 @@ String AT_Buffer;
 // String 轉換成 Char array 緩衝用
 char STR_BUFFER[20];
 
-// String 轉換成 Char array 緩衝用
-uint32_t TOTAL_PL = 0; // 計算RL之前 用來統計整個訊息長度用
-byte MQTT_RL[4];
+char TOPIC[40] = {'\0'};
+char MSG[MSG_SIZE] = {'\0'};
+char ID[20] = {'\0'};
 
 String radom_str; // 給client id 用
 
 uint16_t _loop_count = 0; // 記數用
-
-/***********<< 封包格式 >>**********
-  CONNECT
-  PUBLISH
-*********************************/
-struct PUBLISH
-{
-  uint8_t Fix_Header = 0x30 | 0b0010; // 0x30(publish) | 0b0010 (QoS:1)| 0b0001 (Retain)
-  byte RL[4];
-  uint16_t RL_size;
-
-  byte PL_topic[2];
-  char TOPIC[40] = {'\0'};
-
-  byte PL_msg[2];
-  char MSG[MSG_SIZE] = {'\0'};
-};
-
-struct CONNECT
-{
-  uint8_t Fix_Header = 0x10;
-  byte RL[4];
-  uint16_t RL_size;
-  byte Flags = 0x02; // Connect flags: 0b00000010 (Clean Session)
-  byte Protocol[7] = {
-      0x00, 0x04,                                 // Protocol Length: 4
-      (byte)'M', (byte)'Q', (byte)'T', (byte)'T', // Protocol Name:MQTT
-      0x04                                        // Protocol level (mqtt3.1.1 是0x04)
-  };
-  byte KeepAlive[2] = {0x00, 0x3C}; // Keep Alive: 60 sec
-
-  byte PL_id[2];
-  char ID[20] = {'\0'};
-};
-
-CONNECT CONNECT_KPG;
-PUBLISH PUBLISH_KPG;
-
-/***********<< 資料整理 >>**********
-  ZeroPadding
-*********************************/
-String ZeroHexPadding(byte _value)
-{
-  String _buffer = "";
-
-  if (_value < 16)
-    _buffer = "0" + String(_value, HEX);
-  else
-    _buffer = String(_value, HEX);
-  _buffer.toUpperCase();
-  return _buffer;
-}
-
-void showByteBuffer(byte *_buffer, uint16_t _size, uint8_t _isChar = false)
-{
-  for (uint16_t _i = 0; _i < _size; _i++)
-  {
-    if (_isChar)
-      Serial.print((char)_buffer[_i]);
-    else
-    {
-      Serial.print(F("0x"));
-      Serial.print(ZeroHexPadding(_buffer[_i]));
-      Serial.print(F(" "));
-    }
-
-    if (_i % 10 == 9)
-      Serial.print(F("\r\n"));
-    delay(10);
-  }
-  Serial.println(F("\r\n"));
-}
-
-String CharToHexStr(byte _char, uint8_t _show = false)
-{
-  byte _buffer = (byte)_char;
-  if (_show)
-    Serial.print(ZeroHexPadding(_buffer));
-  return ZeroHexPadding(_buffer);
-}
-
-String ByteToHexStr(byte _byte, uint8_t _show = false)
-{
-  if (_show)
-    Serial.print(ZeroHexPadding(_byte));
-  return ZeroHexPadding(_byte);
-}
-
-/***********<< 封包格式 >>**********
-  CalculateRL 計算RL, 填入MQTT_RL Buffer內，回傳用掉幾個bytes
-*********************************/
-uint8_t CalculateRL(uint32_t _size)
-{
-  uint8_t _index = 0;
-  memset(MQTT_RL, '\0', 4); // 清空 Topic Buffer
-  do
-  {
-    uint8_t _d = _size % 128; // 取餘數 _size & 0b01111111
-    _size /= 128;             // 取除以128後的整數：_size = _size >> 7;
-    if (_size > 0)
-      _d |= 0x80;         // 除128的整數 > 0 時，對_d + 128 (bit[7]為1)
-    MQTT_RL[_index] = _d; // 計算好的 _d 放到 buffer內
-    if (_index < 3)
-      _index++; // 避免寫入buffer爆掉，限制_index不超過3
-  } while (_size > 0);
-  return _index;
-}
 
 /***********<< 更新 BUFFER >>**********
   Flash_STR_BUFFER 更新 STR_BUFFER, 輸入String
@@ -201,89 +94,89 @@ void loop(void)
     _loop_count++;
 
     // 建立Topic
-    memset(PUBLISH_KPG.TOPIC, '\0', 40); // 清空 Topic Buffer
-    strcat(PUBLISH_KPG.TOPIC, MQTT_LASS_Topic);
+    memset(TOPIC, '\0', 40); // 清空 Topic Buffer
+    strcat(TOPIC, MQTT_LASS_Topic);
 
     if (Flash_STR_BUFFER(PMU.Field_3))
-      strcat(PUBLISH_KPG.TOPIC, STR_BUFFER); // 把(String)ID 放到STR_BUFFER中
+      strcat(TOPIC, STR_BUFFER); // 把(String)ID 放到STR_BUFFER中
 
     // 建立 從Field 建立ID 的String
-    memset(CONNECT_KPG.ID, '\0', 20);   // 清空 ID Buffer
-    strcat(CONNECT_KPG.ID, STR_BUFFER); // 沿用 STR_BUFFER裡面的 MAC
-    if (Flash_STR_BUFFER(radom_str))    // 把亂數轉存在STR_BUFFER後面
-      strcat(CONNECT_KPG.ID, STR_BUFFER);
+    memset(ID, '\0', 20);            // 清空 ID Buffer
+    strcat(ID, STR_BUFFER);          // 沿用 STR_BUFFER裡面的 MAC
+    if (Flash_STR_BUFFER(radom_str)) // 把亂數轉存在STR_BUFFER後面
+      strcat(ID, STR_BUFFER);
     // radom_str.toCharArray(STR_BUFFER, radom_str.length() + 1);
 
     // 建立Publish封包
-    memset(PUBLISH_KPG.MSG, '\0', MSG_SIZE);
-    strcat(PUBLISH_KPG.MSG, MSG_INIT);
+    memset(MSG, '\0', MSG_SIZE);
+    strcat(MSG, MSG_INIT);
 
     if (Flash_STR_BUFFER(PMU.Field_1))
     {
-      strcat(PUBLISH_KPG.MSG, "|date=");
-      strcat(PUBLISH_KPG.MSG, STR_BUFFER);
+      strcat(MSG, "|date=");
+      strcat(MSG, STR_BUFFER);
     }
     if (Flash_STR_BUFFER(PMU.Field_2))
     {
-      strcat(PUBLISH_KPG.MSG, "|time=");
-      strcat(PUBLISH_KPG.MSG, STR_BUFFER);
+      strcat(MSG, "|time=");
+      strcat(MSG, STR_BUFFER);
     }
 
     if (Flash_STR_BUFFER(PMU.Field_3))
     {
-      strcat(PUBLISH_KPG.MSG, "|device_id=");
-      strcat(PUBLISH_KPG.MSG, STR_BUFFER);
+      strcat(MSG, "|device_id=");
+      strcat(MSG, STR_BUFFER);
     }
 
     if (Flash_STR_BUFFER(PMU.Field_4))
     {
-      strcat(PUBLISH_KPG.MSG, "|gps_lon=");
-      strcat(PUBLISH_KPG.MSG, STR_BUFFER);
+      strcat(MSG, "|gps_lon=");
+      strcat(MSG, STR_BUFFER);
     }
     if (Flash_STR_BUFFER(PMU.Field_5))
     {
-      strcat(PUBLISH_KPG.MSG, "|gps_lat=");
-      strcat(PUBLISH_KPG.MSG, STR_BUFFER);
+      strcat(MSG, "|gps_lat=");
+      strcat(MSG, STR_BUFFER);
     }
 
     if (Flash_STR_BUFFER(PMU.Field_6))
     {
-      strcat(PUBLISH_KPG.MSG, "|s_ec2=");
-      strcat(PUBLISH_KPG.MSG, STR_BUFFER);
+      strcat(MSG, "|s_ec2=");
+      strcat(MSG, STR_BUFFER);
     }
     if (Flash_STR_BUFFER(PMU.Field_7))
     {
-      strcat(PUBLISH_KPG.MSG, "|s_ph2=");
-      strcat(PUBLISH_KPG.MSG, STR_BUFFER);
+      strcat(MSG, "|s_ph2=");
+      strcat(MSG, STR_BUFFER);
     }
     if (Flash_STR_BUFFER(PMU.Field_8))
     {
-      strcat(PUBLISH_KPG.MSG, "|s_t7=");
-      strcat(PUBLISH_KPG.MSG, STR_BUFFER);
+      strcat(MSG, "|s_t7=");
+      strcat(MSG, STR_BUFFER);
     }
     if (Flash_STR_BUFFER(PMU.Field_9))
     {
-      strcat(PUBLISH_KPG.MSG, "|s_Tb=");
-      strcat(PUBLISH_KPG.MSG, STR_BUFFER);
+      strcat(MSG, "|s_Tb=");
+      strcat(MSG, STR_BUFFER);
     }
 
     if (Flash_STR_BUFFER(PMU.Field_10))
     {
-      strcat(PUBLISH_KPG.MSG, "|bat_v=");
-      strcat(PUBLISH_KPG.MSG, STR_BUFFER);
+      strcat(MSG, "|bat_v=");
+      strcat(MSG, STR_BUFFER);
     }
     if (Flash_STR_BUFFER(PMU.Field_11))
     {
-      strcat(PUBLISH_KPG.MSG, "|bat_a=");
-      strcat(PUBLISH_KPG.MSG, STR_BUFFER);
+      strcat(MSG, "|bat_a=");
+      strcat(MSG, STR_BUFFER);
     }
 
     if (Flash_STR_BUFFER(PMU.Field_12))
     {
-      strcat(PUBLISH_KPG.MSG, "|F12=");
-      strcat(PUBLISH_KPG.MSG, STR_BUFFER);
+      strcat(MSG, "|F12=");
+      strcat(MSG, STR_BUFFER);
     }
-    strcat(PUBLISH_KPG.MSG, "|");
+    strcat(MSG, "|");
 
     // 把要傳送封包(LASS 格式), 打包成String
     // msg_str = "|device=Linkit7697|device_id=9C65F920C020|ver_app=1.1.0|date=2019-03-21|time=06:53:55|tick=0|FAKE_GPS=1|gps_lon=121.787|gps_lat=25.1933|s_ec=200000.00|s_ph=14.00|s_t0=100.00|s_Tb=10000.00|bat_v=3.70|bat_a=400.00|";
@@ -291,30 +184,23 @@ void loop(void)
     /*********** 封包內容印出來確認(不要刪) ***********/
     //     Serial.println("[ID-"+String(_loop_count)+"]");
     //     Serial.print("\t");
-    //     Serial.print(strlen(CONNECT_KPG.ID));
+    //     Serial.print(strlen(ID));
     //     Serial.print(" -> ");
-    //     Serial.println(CONNECT_KPG.ID);
+    //     Serial.println(ID);
 
     /*********** 檢查 PUBLISH_KPG.TOPIC 內容(不要刪) ***********/
     //     Serial.println("[TOPIC-"+String(_loop_count)+"]");
     //     Serial.print("\t");
-    //     Serial.print(strlen(PUBLISH_KPG.TOPIC));
+    //     Serial.print(strlen(TOPIC));
     //     Serial.print(" -> ");
-    //     Serial.println(PUBLISH_KPG.TOPIC);
-
-    // for(byte _i=0;_i<strlen(PUBLISH_KPG.TOPIC);_i++){
-    //   CharToHexStr(PUBLISH_KPG.TOPIC[_i],true);
-    //   if(_i%10==1) Serial.print("\r\n");
-    //   else         Serial.print(" ");
-    // }
-    // Serial.println();
+    //     Serial.println(TOPIC);
 
     /*********** 檢查 PUBLISH_KPG.MSG 內容(不要刪) ***********/
     Serial.println("[MSG-" + String(_loop_count) + "]");
     Serial.print("\t");
-    Serial.print(strlen(PUBLISH_KPG.MSG));
+    Serial.print(strlen(MSG));
     Serial.print(" -> ");
-    Serial.print(PUBLISH_KPG.MSG);
+    Serial.print(MSG);
     Serial.println("\r\n");
 
     PMU.ControlPower(PMU.OFF);
