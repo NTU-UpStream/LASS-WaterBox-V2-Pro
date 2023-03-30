6ff7a134860586a6f9299e579a8754a849306c8e
KyleLiuCat <kyle.liu.coffeecat@gmail.com>
2023-03-30 11:37:29 +0800

    modify comment c

--- Firmware/AllSystem_PMU_V2.3_ublox/AllSystem_PMU_V2.3_ublox.ino
+++ Firmware/AllSystem_PMU_V2.3_ublox/AllSystem_PMU_V2.3_ublox.ino
@@ -24,7 +24,8 @@ uint16_t _sUartBufferSize;
 static char TOPIC[40] = { '\0' };
 static char MSG[MSG_SIZE] = { '\0' };
 static char ID[20] = { '\0' };
-static char radomID[4] = { '_', (char)random(65, 90), (char)random(65, 90), '\0' };
+static char radomID[6] = { '_', (char)random(65, 90), (char)random(65, 90),
+                           (char)random(65, 90), (char)random(65, 90), '\0' };
 
 // String 轉換成 Char array 緩衝用
 static char STR_BUFFER[64];
@@ -45,7 +46,7 @@ void setup(void) {
   PMU.init();
   PMU.setDebuger(Serial);
   PMU.setSleepSec(2);
-  PMU.setWakeUpVolate(3.2);
+  PMU.setWakeUpVolate(3.6);
   PMU.PowerSaveMode(PMU.ON);  // PMU.ON進入睡眠, Delay不睡覺 PMU.OFF
   PMU.setINA219(0x40);
 
@@ -53,8 +54,9 @@ void setup(void) {
   Serial.println(F("-----------------------------------"));
   Serial.println(F("\r\n"));
 
-  // 先睡一次，讓MCU 取得資料並且紀錄
-  PMU.Sleep();
+
+  PMU.Sleep();                // 先睡一次，讓MCU 取得資料並且紀錄
+  Serial.println(F("\r\n"));  // 讓顯示比較清楚一些
 }
 
 void loop(void) {
@@ -62,7 +64,6 @@ void loop(void) {
     PMU.ControlPower(PMU.OFF);
 
     _loop_count++;
-
 
     // 建立Topic
     memset(TOPIC, '\0', 40);  // 清空 Topic Buffer
@@ -74,10 +75,10 @@ void loop(void) {
     strcat(ID, PMU.Field_3);
     strcat(ID, radomID);  // 增加亂數在裡面
 
-    // 建立Publish封包
+    // packaged the publish payload
     memset(MSG, '\0', MSG_SIZE);
     strcat_PF(MSG, MSG_INIT);
-
+    // System Datetime
     if (strlen(PMU.Field_1) > 0) {
       strcat_PF(MSG, PSTR("|date="));
       strcat(MSG, PMU.Field_1);
@@ -86,12 +87,12 @@ void loop(void) {
       strcat_PF(MSG, PSTR("|time="));
       strcat(MSG, PMU.Field_2);
     }
-
+    // Device ID
     if (strlen(PMU.Field_3) > 0) {
       strcat_PF(MSG, PSTR("|device_id="));
       strcat(MSG, PMU.Field_3);
     }
-
+    // Longitude, Latitude
     if (strlen(PMU.Field_4) > 0) {
       strcat_PF(MSG, PSTR("|gps_lon="));
       strcat(MSG, PMU.Field_4);
@@ -100,7 +101,7 @@ void loop(void) {
       strcat_PF(MSG, PSTR("|gps_lat="));
       strcat(MSG, PMU.Field_5);
     }
-
+    // Sensoring items
     if (strlen(PMU.Field_6) > 0) {
       strcat_PF(MSG, PSTR("|s_ec2="));
       strcat(MSG, PMU.Field_6);
@@ -117,6 +118,7 @@ void loop(void) {
       strcat_PF(MSG, PSTR("|s_Tb="));
       strcat(MSG, PMU.Field_9);
     }
+    // Battery status
     if (strlen(PMU.Field_10) > 0) {
       strcat_PF(MSG, PSTR("|bat_v="));
       strcat(MSG, PMU.Field_10);
@@ -125,12 +127,12 @@ void loop(void) {
       strcat_PF(MSG, PSTR("|bat_a="));
       strcat(MSG, PMU.Field_11);
     }
-
+    // 沒有用到的欄位
     if (strlen(PMU.Field_12) > 0) {
       strcat_PF(MSG, PSTR("|F12="));
       strcat(MSG, PMU.Field_12);
     }
-
+    // Reserve
     strcat_PF(MSG, PSTR("|"));
 
     // 把要傳送封包(LASS 格式), 打包成String
@@ -149,9 +151,9 @@ void loop(void) {
     Serial.println(F("\r\n"));
 
     delay(1000);  // 停一秒做7697 及 NBIOT模組供電切換
-    
+
     NBIOT.ON(45);  // 開啟動模組45秒
-    
+
     // 初始化MQTT參數(花時0.8*4 = 3.2sec)
     sprintf_P(STR_BUFFER, PSTR("AT+UMQTT=0,\"%s\""), ID);
     NBIOT.AT_CMD(STR_BUFFER, true);
@@ -167,9 +169,7 @@ void loop(void) {
 
     // 嘗試連線45次, 一次2秒，最多90秒
     for (int8_t _i = 45; _i > 0; _i--) {
-
-      // 用 "AT+CGATT?"測試跟是否加入電信商的網路
-      isOnline = NBIOT.isOnline();
+      isOnline = NBIOT.isOnline();  // 用 "AT+CGATT?"測試跟是否加入電信商的網路
 
       if (!isOnline) {
         Serial.print(_i);
@@ -178,10 +178,10 @@ void loop(void) {
       } else {
         Serial.println(F("[SYS-OK]\tNBIOT is online"));
         NBIOT.AT_CMD("AT+UMQTTC=0", true, 2000);  // 先強制關閉連線(2sec，怕之前的ClientID還在Broker上
-        NBIOT.MQTT_pub(TOPIC, MSG, 1, 1);         // _topic, _msg, _QoS=0, _retain=0
+        NBIOT.MQTT_pub(TOPIC, MSG, 1, 0);         // _topic, _msg, _QoS=0, _retain=0
         _i = -1;                                  // 強制跳出 for
       }
-    }
+    }  // Eno of for 45
 
     NBIOT.OFF();
 
